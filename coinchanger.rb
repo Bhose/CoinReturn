770a9666632fe3b2229302bdb1369639cd3f46c5
Brittney Hose <bhose10@gmail.com>
2017-12-03 11:27:42 -0500

    accounted for quarters

--- coinchanger.rb
+++ coinchanger.rb
@@ -1,5 +1,6 @@
 def coin_checker(change)
 	coin = {}
+
 	if change > 0 && change < 5
 		coin[:penny] = change
 	elsif change > 4 && change < 10
@@ -8,7 +9,8 @@ def coin_checker(change)
 		if change > 0
 			coin[:penny] = change
 		end
-	elsif change > 9
+
+	elsif change > 9 && change < 25
 		coin[:dime] = change / 10
 		change = change % 10
 		if change > 4
@@ -18,7 +20,25 @@ def coin_checker(change)
 				coin[:penny] = change
 			end
 		end
+	
+
+	elsif change > 24 
+		coin[:quarter] = change / 25
+		change = change % 25
+		if change > 9
+			coin [:dime] = change / 10
+			change = change % 10
+			if change > 4 
+				coin[:nickel] = change / 5
+				change = change % 5
+				if change > 0 && change < 4
+					coin[:penny] = change
+				end
+			end
+		end
 	end
+
+	
 	coin
 end
 
--- coinchanger_test.rb
+++ coinchanger_test.rb
@@ -26,6 +26,20 @@ def test_assert_that_10_returns_dime
 	assert_equal({:dime => 1}, coin_checker(change))
 end
 
+def test_assert_that_25_returns_quarter
+	change = 25
+	assert_equal({:quarter => 1}, coin_checker(change))
+end
+
+# def test_assert_that_27_returns_a_quarter_and_two_pennies
+# 	change = 27
+# 	assert_equal({:quarter => 1, :penny => 2}, coin_checker(change))
+# end
+
+def test_assert_that_50_returns_2_quarters
+	change = 50
+	assert_equal({:quarter => 2}, coin_checker(change))
+end
 
 
 end
